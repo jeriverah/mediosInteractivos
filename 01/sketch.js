6047c0a82a0fcc532cbbd608aff71b7ed21a9d01
jeriverah <42587799+jeriverah@users.noreply.github.com>
2018-08-21 14:45:32 -0500

    Add files via upload

---
+++ 01/sketch.js
@@ -0,0 +1,435 @@
+function setup() {
+  createCanvas(600, 150);
+}
+
+function draw() {
+  background(255,255,185);
+	stroke(0);
+	strokeWeight(2);
+	line(357,6 , 465,373);
+	line(357,6 , 264,355);
+	noStroke();
+	fill(255,255,215);
+	triangle(357,7 , 433,271 , 289,271);
+	
+	stroke(0);
+	strokeWeight(1);
+	line(412,0 , 413,73);
+	line(415,0 , 415,73);
+	line(404,14 , 423,14);
+	line(404,16 , 423,16);
+	line(406,66 , 423,65);
+	line(406,67 , 423,66);
+	line(406,69 , 423,68);
+	noFill();
+	arc(420,35, 25, 23, HALF_PI, PI + HALF_PI);
+  
+  stroke(0)
+  strokeWeight(1)
+  fill(51,153,255,100)
+  	beginShape()
+	vertex(114.5,193)
+	vertex(204.5,100.8)
+	vertex(366.4,241.9)
+	vertex(216,288)
+	endShape(CLOSE)
+  
+  stroke(255,255,0)
+  strokeWeight(2)
+  line(422.5,129.3 , 375.4,164.7)
+  stroke(0)
+  strokeWeight(1)
+  line(374.3,164,422,128.4)
+	
+	stroke(250);
+	strokeWeight(4);
+	rect(490,23 , 10,9);
+	stroke(0);
+	strokeWeight(2);
+	fill(255,255,150);
+	rect(490,23 , 10,9);
+	noStroke();
+	fill(255,255,0);
+	triangle(361,51, 387,51, 376,29);
+	stroke(0)
+	strokeWeight(1.5);
+	line(367.3,40 , 371,53);
+	
+	stroke(0);
+	strokeWeight(1);
+	fill(0);
+	ellipse(396,57 , 19,19)
+		stroke(160);
+	strokeWeight(1);
+	fill(160);
+	ellipse(396,57 , 17.5,17.5)
+		stroke(255);
+	strokeWeight(1);
+	fill(255);
+	ellipse(396,57 , 13.5,13.5)
+		stroke(51,0,120);
+	strokeWeight(1);
+	fill(51,0,120);
+	ellipse(396,57 , 12,12)
+	
+		stroke(102,0,204);
+	strokeWeight(1);
+	fill(204,153,255);
+	ellipse(329,35 , 16,16);
+	
+	stroke(0);
+	strokeWeight(1);
+	line(302,36 , 405,100);
+	line(405,100 , 305,60);
+	
+	stroke(0);
+	fill(255,51,51);
+	ellipse(298,80 , 10.3,10.3)
+	
+		stroke(255,255,150);
+	fill(255,255,150);
+	ellipse(357,99 , 23.2,23.2)
+	stroke(210,255,255);
+	fill(210,255,255);
+	ellipse(357,99 , 19,19)
+	stroke(0);
+	fill(0);
+	ellipse(357,99 , 16,16)
+	stroke(235);
+	fill(235);
+	ellipse(357,99 , 15,15)
+	
+	stroke(0)
+	fill(250)
+	ellipse(299.2,121 , 41,41)
+	stroke(0)
+	strokeWeight(3)
+	ellipse(299.2,121 , 26,26)
+		stroke(250,0,0)
+	strokeWeight(1.5)
+	ellipse(299.2,121 , 22,22)
+
+	noStroke()
+	fill(255,170,170)
+	ellipse(82.3,75.3 , 152,152)
+   noStroke()
+  fill(250,20,20)
+  ellipse(123,137 , 45.4,45.4)
+	noStroke()
+	fill(0)
+	ellipse(82.3,75.3 , 120,120)
+  
+	noStroke()
+	fill(51,0,102)
+	ellipse(82.3,75.3 , 60,60)
+  
+	stroke(255,51,51)
+	strokeWeight(2)
+	line(306,164 , 405,112)
+		stroke(0)
+	strokeWeight(1)
+	line(307,164 , 403,113)
+	
+	stroke(0)
+	strokeWeight(2.5)
+	line(295,198 , 356,118)
+	line(356,118 , 443,168)
+		stroke(0,102,51)
+	strokeWeight(1.3)
+	line(295,198 , 356,120)
+	line(356,120 , 443,168)
+	
+	//Color
+		noStroke()
+	fill(250)
+	beginShape()
+	vertex(600,11.2)
+	vertex(600,23)
+	vertex(432.7,106)
+	vertex(427,97)
+	endShape()
+	beginShape()
+	vertex(600,23)
+	vertex(600,33.5)
+	vertex(437.8,113.3)
+	vertex(432.7,106)
+	endShape()
+	beginShape()
+	vertex(600,23)
+	vertex(600,33.5)
+	vertex(437.8,113.3)
+	vertex(432.7,106)
+	endShape()
+	beginShape()
+	vertex(437.8,113.6)
+	vertex(442.3,121.3)
+	vertex(518.2,81.8)
+	vertex(514,73.3)
+	endShape()
+	beginShape()
+	vertex(449.8,113.6)
+	vertex(455.2,126.2)
+	vertex(520.3,91)
+	vertex(515.6,83.1)
+	endShape()
+	beginShape()
+	fill(156,73,0)
+	vertex(469.4,117.7)
+	vertex(554,75.3)
+	vertex(474,125.8)
+	endShape()
+	fill(240)
+	beginShape()
+	vertex(473.5,125.8)
+	vertex(478,133.1)
+	vertex(521,105)
+	vertex(517.7,98.5)
+	endShape()
+	beginShape()
+	vertex(454.3,126.3)
+	vertex(462.5,120.5)
+	vertex(491.7,167.5)
+	vertex(482.2,174.8)
+	endShape()
+	beginShape()
+	vertex(432.2,140.8)
+	vertex(454.5,126.2)
+	vertex(472.3,158)
+	vertex(452.8,171.1)
+	endShape()
+	stroke(0)
+	strokeWeight(1)
+	fill(128)
+	beginShape()
+	vertex(552.9,35)
+	vertex(558,44.5)
+	vertex(569,39)
+	vertex(563.8,29)
+	endShape()
+	fill(215)
+		beginShape()
+	vertex(503.7,59)
+	vertex(509.8,68)
+	vertex(520,63)
+	vertex(514.4,55)
+	endShape()
+	fill(100)
+	beginShape()
+	vertex(460,82)
+	vertex(468.5,77)
+	vertex(476.8,85)
+	vertex(465.7,90)
+	endShape(CLOSE)
+		beginShape()
+	fill(0)
+	vertex(543,62)
+	vertex(551.8,58)
+	vertex(556.5,65)
+	vertex(547.8,69.8)
+	endShape(CLOSE)
+		beginShape()
+	vertex(435.5,94)
+	vertex(440.6,103)
+	vertex(448.8,98)
+	vertex(443.5,90)
+	endShape(CLOSE)
+	fill(204,0,0)
+	
+		beginShape()
+	vertex(427.8,98)
+	vertex(435.3,94)
+	vertex(442,103)
+	vertex(433,108)
+	endShape(CLOSE)
+	fill(102,102,0)
+		beginShape()
+	vertex(456,94)
+	vertex(460.5,103)
+	vertex(471,97)
+	vertex(465.5,90)
+	endShape(CLOSE)
+		beginShape()
+	fill(150)
+	vertex(471.7,97)
+	vertex(477.2,104)
+	vertex(492,96)
+	vertex(484.5,90.8)
+	endShape(CLOSE)
+	noStroke()
+		beginShape()
+	fill(190)
+	vertex(432.7,107)
+	vertex(440,102.8)
+	vertex(444.8,109.2)
+	vertex(437.7,115.5)
+	endShape(CLOSE)
+		beginShape()
+	fill(0)
+	vertex(445,109.1)
+	vertex(453.2,104.7)
+	vertex(459.6,115)
+	vertex(450,120)
+	endShape(CLOSE)
+		beginShape()
+	vertex(442.2,121.2)
+	vertex(450.5,118)
+	vertex(455.5,128)
+	vertex(447.7,132)
+	endShape(CLOSE)
+		beginShape()
+	vertex(516,83.1)
+	vertex(520,92)
+	vertex(533,85)
+	vertex(528.2,76.5)
+	endShape(CLOSE)
+		beginShape()
+	vertex(541.8,83.8)
+	vertex(548.5,80)
+	vertex(552.9,87)
+	vertex(546.2,91)
+	endShape(CLOSE)
+		beginShape()
+	vertex(547,93.6)
+	vertex(554.9,89.2)
+	vertex(559.5,97.3)
+	vertex(552.1,102)
+	endShape(CLOSE)
+	stroke(0)
+	fill(250)
+	ellipse(569,83.1,21.5,21.5)
+  noStroke()
+  fill(130)
+  beginShape()
+	vertex(447.8,132.2)
+	vertex(454.4,127.7)
+	vertex(459,134.7)
+	vertex(452.5,139)
+	endShape(CLOSE)
+  beginShape()
+beginShape()
+	vertex(432.8,141.8)
+	vertex(441.8,136.8)
+	vertex(446,143.2)
+	vertex(438,148.3)
+	endShape(CLOSE)
+  fill(0)
+  beginShape()
+	vertex(442.7,155.7)
+	vertex(450.8,151)
+	vertex(455.8,158.8)
+	vertex(447.7,163.9)
+	endShape(CLOSE)
+  fill(180)
+  beginShape()
+	vertex(446,143.7)
+	vertex(452.8,139)
+	vertex(457,147.5)
+	vertex(451.6,151.7)
+	endShape(CLOSE)
+  fill(0)
+    beginShape()
+	vertex(506.8,114)
+	vertex(520,105.3)
+	vertex(528,119)
+	vertex(515.2,127.4)
+	endShape(CLOSE)
+  	
+	//Malla
+	stroke(0)
+	strokeWeight(1)
+	line(427,97 , 526.5,251.5)
+	line(427,97 , 600,11.2)
+	line(432.7,106 , 600,23)
+	line(437.8,113.3 , 600,33.5)
+	line(442.8,122 , 528,76.3)
+	line(435.3,92.3 , 526.5,251.5)
+	line(443,88.4 , 539,245.8)
+	line(451,84.3 , 539,245.8)
+	line(433,142 , 468.8,118)
+	line(433,142 , 587.5,380.4)
+	line(441,135.5 , 531,275.6)
+	line(448,131 , 531,275.6)
+	line(469,117.5 , 572.5,63.8)
+	line(572.5,63.8 , 438,148.2)
+	line(443,156 , 520.2,105.5)
+	line(447.5,163.5 , 467.7,149.9)
+	line(452.8,171.3 , 476.4,155.2)
+  
+  line(311,148.5 , 372.5,118.5)
+  line(311.7,150.5 , 373.5,120)
+  
+  line(203.8,35.8 , 203.8,142)
+  line(187.5,73 , 224,72.8)
+  line(191,81 , 228.3,80)
+  line(180,39 , 254.3,73)
+  
+  //Rosa
+	noStroke()
+	fill(213,59,141,150)
+	rect(414,89 , 33.3,25)
+  
+  fill(204,204,0,150)
+  ellipse(527.6,71.7,27.2,27.2)
+  fill(51,51,255,150)
+  ellipse(527.6,71.7,22.2,22.2)
+  
+  fill(255,0,0,180)
+  ellipse(528.5,105.9,27.1,27.1)
+  
+  fill(255,153,204,150)
+  ellipse(527.7,105,24,24)
+  
+  stroke(0)
+  strokeWeight(1)
+  fill(204,204,0,150)
+  ellipse(456.2,83.8,39.8,39.8)
+  
+  noStroke()
+  fill(255,128,0,150)
+  ellipse(464.4,84.1,34.6,34.6)
+  
+  
+  fill(0,255,255,150)
+  ellipse(461.2,84.2,29.2,34.7)
+  
+  stroke(0)
+  line(358.7,157 , 421,103)
+  line(341,185 , 428,104.5)
+  
+  fill(0)
+  	beginShape()
+	vertex(518,132.2)
+	vertex(531,124)
+	vertex(536.5,133.3)
+	vertex(524,142)
+	endShape(CLOSE)
+  noStroke()
+  	beginShape()
+	vertex(495,164.8)
+	vertex(497.4,168.4)
+  vertex(564,119)
+	vertex(561,116)
+  endShape()
+  fill(204,102,0)
+  beginShape()
+	vertex(472.7,136.5)
+	vertex(477.5,133.3)
+  vertex(536.4,241.3)
+	endShape(CLOSE)
+  
+  noStroke()
+  fill(0)
+   beginShape()
+	vertex(432,145.5)
+	vertex(435,149)
+  vertex(424,161)
+  vertex(422,159)
+	endShape(CLOSE)
+   beginShape()
+	vertex(282,167)
+	vertex(307,140.5)
+  vertex(311.5,144.7)
+  vertex(287.5,172)
+	endShape(CLOSE)
+  
+}
